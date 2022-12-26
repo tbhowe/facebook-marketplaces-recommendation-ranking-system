3f7d66c48a1229ee70bcfec9f0759dd6485ce4b3
Tim Howe <tbhowe@gmail.com>
2022-12-26 08:08:36 +0000

    refactors train.py main train function, removes comments, moves validation to end of epoch loop

--- train.py
+++ train.py
@@ -15,8 +15,8 @@ def train(
     train_loader,
     val_loader,
     test_loader,
-    lr=0.1,
-    epochs=100,
+    lr=0.0001,
+    epochs=50,
     optimiser=torch.optim.SGD
 ):
     """
@@ -24,55 +24,51 @@ def train(
 
     Parameters:
     - model: a pytorch model
-    - dataloader: a pytorch dataloader
+    - train_loader, val_loader, test_loader - pytorch dataloaders for training, validation, and test sets
+    - lr - master learning rate (default 0,0001)
+    - eochs - number of epochs (default 50)
+    - optimiser - optimiser used for updating weights, (default: Stochastic Gradient Descent, SGD)
+  
 
     Returns:
     - model: a trained pytorch model
     """
 
-    # components of a ml algortithms
-    # 1. data
-    # 2. model
-    # 3. criterion (loss function)
-    # 4. optimiser
-
     writer = SummaryWriter()
 
-    # initialise an optimiser
+    # initialise optimiser, learning rate scheduler, iteration variables
     optimiser = optimiser(model.parameters(), lr=lr, weight_decay=0.001)
     scheduler = lr_scheduler.MultiStepLR(optimiser, milestones=[5,20,50], gamma=0.1,verbose=True)
     state_dict=torch.load( 'model_evaluation/TransferLearning2022-12-25-08:15:53/saved_weights/_latest_weights.pt' )
     model.load_state_dict(state_dict)
     batch_idx = 0
     epoch_idx= 0
+
     for epoch in range(epochs):  # for each epoch
-        # 
-        
         print('Epoch:', epoch_idx,'LR:', scheduler.get_lr())
         weights_filename=model.weights_folder_name + '_' + str(epoch) + '_latest_weights.pt'
         epoch_idx +=1
         torch.save(model.state_dict(), weights_filename)
-        for batch in train_loader:  # for each batch in the dataloader
+        for batch in train_loader:  
             features, labels = batch
-            prediction = model(features)  # make a prediction
-            # compare the prediction to the label to calculate the loss (how bad is the model)
+            prediction = model(features)  
             loss = F.cross_entropy(prediction, labels)
-            loss.backward()  # calculate the gradient of the loss with respect to each model parameter
-            optimiser.step()  # use the optimiser to update the model parameters using those gradients
+            loss.backward()
+            optimiser.step()
             print("Epoch:", epoch, "Batch:", batch_idx,
                   "Loss:", loss.item())  # log the loss
             optimiser.zero_grad()  # zero grad
             writer.add_scalar("Loss/Train", loss.item(), batch_idx)
             batch_idx += 1
-            if batch_idx % 100 == 0:
-                print('Evaluating on valiudation set')
-                # evaluate the validation set performance
-                val_loss, val_acc = evaluate(model, val_loader)
-                writer.add_scalar("Loss/Val", val_loss, batch_idx)
-                writer.add_scalar("Accuracy/Val", val_acc, batch_idx)
 
+            
+        print('Evaluating on valiudation set')
+        # evaluate the validation set performance
+        val_loss, val_acc = evaluate(model, val_loader)
+        writer.add_scalar("Loss/Val", val_loss, batch_idx)
+        writer.add_scalar("Accuracy/Val", val_acc, batch_idx)
         scheduler.step()
-    # evaluate the final test set performance
+    
     
     print('Evaluating on test set')
     test_loss = evaluate(model, test_loader)
