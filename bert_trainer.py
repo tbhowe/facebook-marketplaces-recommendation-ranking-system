2eb28ef7789d515daa17a5f2d64702c8613d04ed
Tim Howe <tbhowe@gmail.com>
2023-01-09 10:37:38 +0000

    completes definition of TrainingConfig class in bert_trainer.py

--- bert_trainer.py
+++ bert_trainer.py
@@ -1,3 +1,4 @@
+#%%
 from bert_dataset import TextDataset
 from datasets import load_dataset
 from datasets import load_metric
@@ -13,6 +14,8 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 class TrainingCongfig:
+    '''class containing the model definition, training arguments, and training-related methods'''
+    
     def __init__(self):
         self.dataset=TextDataset()
         self.model_path = 'bert-base-uncased'
@@ -25,3 +28,48 @@ class TrainingCongfig:
                                                     id2label={str(i): c for i, c in enumerate(self.labels)},
                                                     label2id={c: str(i) for i, c in enumerate(self.labels)}
                                                 )
+
+        self.training_args = TrainingArguments(  
+                                    output_dir="./bert-classifier",
+                                    per_device_train_batch_size=16,
+                                    evaluation_strategy="steps",
+                                    num_train_epochs=6,
+                                    # fp16=True,
+                                    save_steps=100,
+                                    eval_steps=100,
+                                    logging_steps=10,
+                                    learning_rate=2e-4,
+                                    save_total_limit=2,
+                                    remove_unused_columns=False,
+                                    push_to_hub=False,
+                                    report_to='tensorboard',
+                                    load_best_model_at_end=True,
+                                    )
+        self.trainer = Trainer(
+                    model=self.model,
+                    args=self.training_args,
+                    data_collator=self.collate_function,
+                    compute_metrics=self.compute_metrics,
+                    train_dataset=self.prepared_ds["train"],
+                    eval_dataset=self.prepared_ds["validation"],
+                    tokenizer=self.feature_extractor,
+                    )
+
+    @staticmethod  
+    def collate_function(batch):
+        '''collates examples into a batch'''
+        return {
+            'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
+            'labels': torch.tensor([x['labels'] for x in batch])
+        }
+    
+    def transform(self,example_batch):
+        '''applies the feature extractor to a batch, returing pytorch tensors'''
+        inputs = self.feature_extractor([x for x in example_batch['features']], return_tensors='pt')
+        # include the labels
+        inputs['labels'] = example_batch['label']
+        return inputs
+
+    def compute_metrics(self,p):
+        '''method to compute the metrics on the predictions'''
+        return self.metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
