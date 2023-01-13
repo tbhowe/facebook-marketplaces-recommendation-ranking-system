9d9f985910a651fcc421d36d5f46c2fd16e41c4d
Tim Howe <tbhowe@gmail.com>
2023-01-13 10:18:49 +0000

    working BERT train loop

--- bert_train_script.py
+++ bert_train_script.py
@@ -1,19 +1,117 @@
 #%%
-from bert_trainer import TrainingCongfig
+from bert_dataset import TextDataset
+from bert_trainer import BERT_Classifier
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch
 from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+from torch.utils.data import random_split
+from torchvision import transforms
+from torch.optim import lr_scheduler
 
-training_config = TrainingCongfig()
+torch.manual_seed=123
 
-train_test=training_config.dataset.dataset['train']
-# train_test[1:10]['features']
+def train(
+    model,
+    train_loader,
+    val_loader,
+    test_loader,
+    lr=0.001,
+    epochs=20,
+    optimiser=torch.optim.Adam
+    ):
+    '''train loop for 1d Conv of BERT outputs, with lr scheduling'''
+    writer = SummaryWriter()
 
-train_results = training_config.trainer.train()
-training_config.trainer.save_model()
-training_config.trainer.log_metrics("train", train_results.metrics)
-training_config.trainer.save_metrics("train", train_results.metrics)
-training_config.trainer.save_state()
+    # initialise an optimiser
+    optimiser = optimiser(model.parameters(), lr=lr)  # weight_decay=0.001
+    scheduler = lr_scheduler.MultiStepLR(optimiser, milestones=[5,10], gamma=0.1,verbose=True)
+    batch_idx = 0
+    epoch_idx= 0
+    for epoch in range(epochs):  # for each epoch
+        # 
+        
+        print('Epoch:', epoch_idx,'LR:', scheduler.get_lr())
+        weights_filename=model.weights_folder_name + '_latest_weights.pt'
+        epoch_idx +=1
+        torch.save(model.state_dict(), weights_filename)
 
-# metrics = training_config.trainer.evaluate(training_config.prepared_ds['validation'])
-# training_config.trainer.log_metrics("eval", metrics)
-# training_config.trainer.save_metrics("eval", metrics)
+        for batch in train_loader:  # for each batch in the dataloader
+            features, labels = batch
+            # print(size(images))
+            prediction = model(features)  # make a prediction
+            # compare the prediction to the label to calculate the loss (how bad is the model)
+            loss = F.cross_entropy(prediction, labels)
+            loss.backward()  # calculate the gradient of the loss with respect to each model parameter
+            optimiser.step()  # use the optimiser to update the model parameters using those gradients
+            print("Epoch:", epoch, "Batch:", batch_idx,
+                  "Loss:", loss.item())  # log the loss
+            optimiser.zero_grad()  # zero grad
+            writer.add_scalar("Loss/Train", loss.item(), batch_idx)
+            batch_idx += 1
+            
+        print('Evaluating on valiudation set')
+        # evaluate the validation set performance
+        val_loss, val_acc = evaluate(model, val_loader)
+        writer.add_scalar("Loss/Val", val_loss, batch_idx)
+        writer.add_scalar("Accuracy/Val", val_acc, batch_idx)
+        scheduler.step()
+    # evaluate the final test set performance
+    
+    print('Evaluating on test set')
+    test_loss = evaluate(model, test_loader)
+    # writer.add_scalar("Loss/Test", test_loss, batch_idx)
+    model.test_loss = test_loss
+    
+    return model   # return trained model
+    
+
+def evaluate(model, dataloader):
+    losses = []
+    correct = 0
+    n_examples = 0
+    for batch in dataloader:
+        features, labels = batch
+        prediction = model(features)
+        loss = F.cross_entropy(prediction, labels)
+        losses.append(loss.detach())
+        correct += torch.sum(torch.argmax(prediction, dim=1) == labels)
+        n_examples += len(labels)
+    avg_loss = np.mean(losses)
+    accuracy = correct / n_examples
+    print("Loss:", avg_loss, "Accuracy:", accuracy.detach().numpy())
+    return avg_loss, accuracy
+
+
+if __name__ == "__main__":
+
+    dataset = TextDataset()
+    train_set_len = round(0.8*len(dataset))
+    val_set_len = round(0.1*len(dataset))
+    test_set_len = len(dataset) - val_set_len - train_set_len
+    split_lengths = [train_set_len, val_set_len, test_set_len]
+    # split the data to get validation and test sets
+    train_set, val_set, test_set = random_split(dataset, split_lengths)
+
+    batch_size = 16
+    train_loader = DataLoader(train_set, shuffle=True, batch_size=batch_size)
+    val_loader = DataLoader(val_set, batch_size=batch_size)
+    test_loader = DataLoader(test_set, batch_size=batch_size)
+    # nn = NeuralNetworkClassifier()
+    # cnn = CNN()
+    model = BERT_Classifier()
+    
+    train(
+        model,
+        train_loader,
+        val_loader,
+        test_loader,
+        epochs=20,
+        lr=0.001,
+        optimiser=torch.optim.AdamW
+        )
+
+
+
 # %%
--- bert_trainer.py
+++ bert_trainer.py
@@ -1,10 +1,12 @@
 #%%
 from bert_dataset import TextDataset
-from transformers import BertModel
 from torch import nn
+import time
+import os
 
 
 class BERT_Classifier(nn.Module):
+    ''' 1d convolutional network, taking in BERT model last hidden layer as input'''
     def __init__(self,
                  input_size: int = 768,
                  num_classes: int = 13):
@@ -12,17 +14,35 @@ class BERT_Classifier(nn.Module):
         self.layers = nn.Sequential(nn.Conv1d(input_size, 256, kernel_size=3, stride=1, padding=1),
                                   nn.ReLU(),
                                   nn.MaxPool1d(kernel_size=2, stride=2),
-                                  nn.Conv1d(256, 32, kernel_size=3, stride=1, padding=1),
+                                  nn.Conv1d(256, 128, kernel_size=3, stride=1, padding=1),
+                                  nn.ReLU(),
+                                  nn.MaxPool1d(kernel_size=2, stride=2),
+                                  nn.Conv1d(128, 64, kernel_size=3, stride=1, padding=1),
+                                  nn.ReLU(),
+                                  nn.MaxPool1d(kernel_size=2, stride=2),
+                                  nn.Conv1d(64, 32, kernel_size=3, stride=1, padding=1),
                                   nn.ReLU(),
                                   nn.Flatten(),
-                                  nn.Linear(384 , 128),
-                                  nn.ReLU(),
-                                  nn.Linear(128, num_classes))
+                                  nn.Linear(192 , num_classes)
+                                  )
+                                 
+
+        self.initialise_weights_folders()
+
     def forward(self, input):
         x = self.layers(input)
         return x
+    
+    def initialise_weights_folders(self):
+        ''' method to create folder for saved weights'''
+        start_time = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime())
+        folder_name=str('BERT_1d_conv'+ start_time)
+        if not os.path.exists('model_evaluation/' + folder_name + '/saved_weights/'):
+            os.makedirs('model_evaluation/' + folder_name + '/saved_weights/') 
+        self.weights_folder_name='model_evaluation/' + folder_name + '/saved_weights/'
 
 if __name__ == "__main__":
     test_classifier=BERT_Classifier()
+    params=test_classifier.parameters()
 
 # %%
