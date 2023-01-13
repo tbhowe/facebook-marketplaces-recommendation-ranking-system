6fd650ede99dab25eb580caf1c753692bf5e686b
Tim Howe <tbhowe@gmail.com>
2023-01-13 07:12:03 +0000

    switches bert_dataset.py TextDataset class from HF datasets to torch.utils.data.Dataset type class

--- bert_dataset.py
+++ bert_dataset.py
@@ -3,41 +3,21 @@
 import pandas as pd
 from datasets import Dataset
 from transformers import BertTokenizer
+from transformers import BertModel
 from transformers import DataCollatorForTokenClassification
+import torch
 
-class TextDataset():
+class TextDataset(Dataset):
     '''Class which defines the dataset from the csv of products in the Facebok Marketplace project
-
-    index is currently image, to facilitate comparison with image processor. 
-    However an alternative architecture is to run BERT on products CSV, repopulate with BERT outputs and then join with images.
-    We can decide on this later in the development process.
     '''
 
     def __init__(self):
-        super().__init__()
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-        self.load_dataframe()
+        self.text_df=self.load_dataframe()
         self.categories=self.text_df['labels'].unique()
-         # create dict of cat_name to IDX
-        self.category_name_to_idx = {
-            category: cat_idx for cat_idx, category in enumerate(self.categories)
-        }
-        # create dict of IDX to cat_name
-        self.idx_to_category_name= {
-            value: key for
-            key, value in self.category_name_to_idx.items()
-        }
+        self.encoder = {y: x for (x, y) in enumerate(set(self.categories))}
+        self.decoder = {x: y for (x, y) in enumerate(set(self.categories))}
         
-        self.dataset=Dataset.from_pandas(self.text_df,preserve_index=False) # 
-        self.tokenized_data =self.dataset.map(self.tokenise_function, batched=True)
-        self.tokenized_data = self.tokenized_data.remove_columns(['description'])
-        # self.dataset=self.dataset.train_test_split(test_size=0.3)
-        # self.splitter=self.dataset['test'].train_test_split(test_size=0.5)
-        # self.dataset['test']=self.splitter['test']
-        # self.dataset['validation']=self.splitter['train']
-        # self.transform=transform
-
-       
 
     def load_dataframe(self):
         '''loads the products csv from the Facebook Marketplace porject into a pandas Dataframe.
@@ -47,19 +27,37 @@ class TextDataset():
         text_df=pd.read_csv('images.csv',lineterminator='\n', index_col=0)
         self.text_df=text_df.merge(product_df, left_on ='product_id', right_on='id')
         self.text_df['cat_L1'] = [catter.split("/")[0] for catter in self.text_df['category']]
-        self.text_df=pd.DataFrame().assign(description=self.text_df['product_description'], labels=self.text_df['cat_L1'])
+        return pd.DataFrame().assign(description=self.text_df['product_description'], labels=self.text_df['cat_L1'])
     
-    
-    def tokenise_function(self,example):
-        return self.tokenizer(example['description'], truncation=True)
+    def __getitem__(self, index):
+        label = self.text_df['labels'][index]
+        label = self.encoder[label]
+        label = torch.as_tensor(label)
+        description =self.text_df['description'][index]
+        features=self.tokenizer([description], padding='max_length', truncation=True)
+        features = {key:torch.LongTensor(value) for key, value in features.items()}
+
+        return features, label
+
+    def __len__(self):
+        return self.text_df.shape[0]
+
+
+    # def tokenise_function(self,example):
+    #     return self.tokenizer(example['description'], truncation=True)
         
 
         
     
 if __name__ == "__main__":
     text_dataset=TextDataset()
-    # text_dataset.text_df.head(5)
-    test_out=text_dataset.tokenized_data
-    print(test_out[1])
-
+    test_features, test_label=text_dataset[1]
+    # print(test_features)
+    # print(test_label)
+    
+    model=BertModel.from_pretrained('bert-base-uncased', output_hidden_states = True)
+    model.eval()
+    test_out=model(**test_features).last_hidden_state.swapaxes(1,2)
+    print(test_out)
+    print(len(text_dataset))
 # %%
